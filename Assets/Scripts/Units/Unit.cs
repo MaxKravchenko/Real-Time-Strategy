f585c509cbfe461eb4be224a8ce8d4b9a645e310
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-06-04 15:30:26 +0300

    Storing Our Units

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -10,11 +11,31 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+    
     #region Client
     
     [Client]
@@ -31,6 +52,20 @@ public class Unit : NetworkBehaviour
         if (!isOwned) { return; }
         
         onDeselected?.Invoke();
+    }
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !isOwned) { return; }
+        
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !isOwned) { return; }
+        
+        AuthorityOnUnitDespawned?.Invoke(this);
     }
 
     #endregion
