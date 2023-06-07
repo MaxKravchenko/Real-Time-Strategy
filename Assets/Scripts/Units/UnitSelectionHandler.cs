3fadba486111be0e6a5a79bf71c9d3964dcbc719
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-06-07 10:06:13 +0300

    Improving Selection & Movement

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -27,7 +27,7 @@ public class UnitSelectionHandler : MonoBehaviour
     {
         if (player == null)
         {
-            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+            //player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
         }
         
         if (Mouse.current.leftButton.wasPressedThisFrame)
@@ -47,12 +47,15 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void StartSelectionArea()
     {
-        foreach (Unit selectedUnit in SelectedUnits)
+        if(!Keyboard.current.leftShiftKey.isPressed)
         {
-            selectedUnit.Deselect();
-        }
+            foreach (Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Deselect();
+            }
         
-        SelectedUnits.Clear();
+            SelectedUnits.Clear();
+        }      
 
         unitSelectionArea.gameObject.SetActive(true);
 
@@ -102,6 +105,8 @@ public class UnitSelectionHandler : MonoBehaviour
 
         foreach(Unit unit in player.GetMyUnits())
         {
+            if (SelectedUnits.Contains(unit)) { continue; }
+            
             Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
 
             if(screenPosition.x > min.x &&
