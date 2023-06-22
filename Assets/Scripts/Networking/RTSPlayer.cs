b15c21549909ff99497d1a9c08d4a8ec22b7bee8
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-06-22 09:53:30 +0300

    Team Colors

--- Assets/Scripts/Networking/RTSPlayer.cs
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -15,9 +15,15 @@ public class RTSPlayer : NetworkBehaviour
     
     public event Action<int> ClientOnResourcesUpdated;
     
+    private Color teamColor = new Color();
     private List<Unit> myUnits = new List<Unit>();
     private List<Building> myBuildings = new List<Building>();
 
+    public Color GetTeamColor()
+    {
+        return teamColor;
+    }
+    
     public int GetResources()
     {
         return resources;
@@ -31,12 +37,6 @@ public class RTSPlayer : NetworkBehaviour
     public List<Building> GetMyBuildings()
     {
         return myBuildings;
-    }
-
-    [Server]
-    public void SetResources(int newResources)
-    {
-        resources = newResources;
     }
     
     public bool CanPlaceBuilding(BoxCollider buildingCollider, Vector3 point)
@@ -80,6 +80,18 @@ public class RTSPlayer : NetworkBehaviour
         Building.ServerOnBuildingDespawned -= ServerHandleBuildingDespawned;
     }
 
+    [Server]
+    public void SetTeamColor(Color newTeamColor)
+    {
+        teamColor = newTeamColor;
+    }
+
+    [Server]
+    public void SetResources(int newResources)
+    {
+        resources = newResources;
+    }
+    
     [Command]
     public void CmdTryPlaceBuilding(int buildingId, Vector3 point)
     {
